7c36083f9835066174cb8fdb3bc5c257cc6464c1
Uladzimir.Dzhurko <vladimir.dzhurko@lacit.net>
2021-09-09 20:29:02 +0300

    roll back

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import App from './App'
 
-ReactDom.render(<App />, document.getElementById('root'))
+const AppList = () => {
+    return (
+        <ul>
+            <li>test 1</li>
+            <li>test 2</li>
+        </ul>
+    )
+}
+
+const AppHeader = () => {
+    return <h1>Hello World!</h1>;   
+}
+
+const el = (
+    <>
+        <AppHeader />
+        <AppList />
+    </>
+)
+// console.dir(document.createElement('h1'))
+
+ReactDom.render(el, document.getElementById('root'))
